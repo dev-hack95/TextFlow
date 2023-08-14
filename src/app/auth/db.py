e89d4272e9f8498d2b87e4eac68b008299521106
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-08-15 01:20:52 +0530

    Push

--- src/app/auth/db.py
+++ src/app/auth/db.py
@@ -4,22 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from logger import logging
 from dotenv import load_dotenv
 from exception import CustomException
-import socket
 import sys
-import os
 
 load_dotenv("./.env")
 #ip_addr = socket.gethostbyname(socket.gethostname())
-
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
-
-engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}")
+engine = create_engine("postgresql://postgres:postgres@192.168.29.217/auth_service")
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
